19e745388ec2f64b5a02a857c1ecfdaec5b2e008
ShabbarKaz <114262354+ShabbarKaz@users.noreply.github.com>
2023-12-09 02:14:16 -0500

    chore: added skeleton code file

---
+++ VexRobotics_UWO.py
@@ -0,0 +1,54 @@
+#
+# Authors: 
+#
+# 
+#--------------------------------------------   
+#Authors : 
+#
+#Description : this program is used to control the VexRobotics robot for over undercompetition taking place on january 13 in MSOE
+#
+#--------------------------------------------
+
+#--------------------------------------------
+#
+#wiring guide : basically relevant parts list for the code
+#
+#
+#
+##--------------------------------------------
+#
+#
+#
+
+#
+# The purpose of this method is to move the robot in a straigt line either to the front or to the back
+#we need to figure out how to do implement this method
+#
+def goto(X_Cord,Y_Cord,Speed,Wait_time):
+    print("going to X_Cord,Y_Cord")
+    
+#
+# The purpose of this method is to move allow for the robot to turn it in a certain angle 
+#we need to figure out how to do implement this method
+#    
+    
+def turn(turn_angle):
+    print("turning")
+
+#
+# The purpose of this method is to move the robot in a straigt line either to the front or to the back
+# #we need to figure out how to do implement this method
+#       
+def autonomous():  
+    print("autonomous")
+    
+#
+# The purpose of this method is to move the robot in a straigt line either to the front or to the back
+# #we need to figure out how to do implement this method 
+#       
+def usercontrolled():
+    print("usercontrolled")
+    
+#
+# 
+# the further methods will be determined as we go along/get to know about the design of the robot itself    
